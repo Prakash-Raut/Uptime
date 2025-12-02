f41812f10d41c71d4dc3b3d36eecbb16652c0203
Prakash-Raut <prakash_raut@outlook.com>
2025-12-02 12:58:05 +0530

    chore: enhance API production start script and improve server configuration
    - Updated the production start script in package.json to include a build step before starting the API.
    - Refactored the server listening logic to use a configurable port from environment variables, improving flexibility.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,7 @@
 import app from "./app";
 
-app.listen(3000, () => {
-	console.log("API is live on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`API is live on http://localhost:${PORT}`);
 });
